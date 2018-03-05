705eb340e9a571205d401beb89052575c29dfdbe
Eddy Matlock-Mahon <36487159+eddymahon82@users.noreply.github.com>
2018-03-05 15:44:36 -0800

    Added Coffee theme

---
+++ themes/Coffee/footer.php
@@ -0,0 +1,20 @@
+                <!-- footer starts here -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="footer text-faded text-center py-5">
+      <div class="container">
+        <p class="m-0 small">Copyright &copy; <?php echo date("Y"); ?></p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
