5e0bc264f27944d048610b8521e487691dba96d1
Eddy Matlock-Mahon <36487159+eddymahon82@users.noreply.github.com>
2018-03-05 12:01:55 -0800

    Updated version of Widget 2 files w/ working theme

--- widgets2/includes/config.php
+++ widgets2/includes/config.php
@@ -1,12 +1,30 @@
 <?php
 /*
 config.php
-put configuration info here.....
+
+stores configuration information for our web application
+
 */
 
-define('DEBUG',TRUE); #we want to see all errors
+//removes header already sent errors
+ob_start();
 
-define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
+define('SECURE',false); #force secure, https, for all site pages
+
+define('PREFIX', 'widgets_wn18_'); #Adds uniqueness to your DB table names.  Limits hackability, naming collisions
+
+header("Cache-Control: no-cache");header("Expires: -1");#Helps stop browser & proxy caching
+
+define('DEBUG',true); #we want to see all errors
+
+include 'credentials.php';//stores database info
+include 'common.php';//stores favorite functions
+
+//prevents date errors
+date_default_timezone_set('America/Los_Angeles');
+
+//create config object
+$config = new stdClass;
 
 $nav1['index.php'] = "HOME";
 $nav1['customers.php'] = "CUSTOMERS";
@@ -14,85 +32,92 @@ $nav1['olySummerSports_list.php'] = "OLYMPIC SUMMER SPORTS";
 $nav1['email2.php'] = "CONTACT";
 $nav1['daily.php'] = "DAILY";
 
+//create default page identifier
+define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
 
-//defaults for header.php
-$banner = 'WORLD OF WIDGETS';
-$slogan = 'Our widgets are better than yours!!!!';
+//START NEW THEME STUFF - be sure to add trailing slash!
+$sub_folder = 'widgets2/';//change to 'widgets' or 'sprockets' etc.
+$config->theme = 'BusinessCasual';//sub folder to themes
+
+//will add sub-folder if not loaded to root:
+$config->physical_path = $_SERVER["DOCUMENT_ROOT"] . '/' . $sub_folder;
+//force secure website
+if (SECURE && $_SERVER['SERVER_PORT'] != 443) {#force HTTPS
+	header("Location: https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+}else{
+    //adjust protocol
+    $protocol = (SECURE==true ? 'https://' : 'http://'); // returns true
+    
+}
+$config->virtual_path = $protocol . $_SERVER["HTTP_HOST"] . '/' . $sub_folder;
+
+define('ADMIN_PATH', $config->virtual_path . '/admin/'); # Could change to sub folder
+define('INCLUDE_PATH', $config->physical_path . '/includes/');
+
+//END NEW THEME STUFF
+
+//set website defaults
+$config->title = THIS_PAGE;
+$config->banner = 'Widgets';
+$config->loadhead = '';//place items in <head> element
 
 switch(THIS_PAGE){
         
-    case 'template.php':
-        $title = 'Template Page';
+    case 'contact.php':    
+        $config->title = 'Contact Page';    
+    break;
+    
+    case 'appointment.php':    
+        $config->title = 'Appointment Page';
+        $config->banner = 'Widget Appointments!';
     break;
         
-    default:
-        $title = THIS_PAGE;
+   case 'template.php':    
+        $config->title = 'Template Page';    
+    break;    
+        
         
 }
 
-//other include files referenced here
-include 'credentials.php';
-
-function myerror($myFile, $myLine, $errorMsg)
-{
-    if(defined('DEBUG') && DEBUG)
-    {
-       echo "Error in file: <b>" . $myFile . "</b> on line: <b>" . $myLine . "</b><br />";
-       echo "Error Message: <b>" . $errorMsg . "</b><br />";
-       die();
-    }else{
-		echo "I'm sorry, we have encountered an error.  Would you like to buy some socks?";
-		die();
-    }
-}
-
-function coffee_links($nav1){
-    
-    foreach($nav1 as $url => $text){
-    //echo '<li><a href="' . $url . '">' . $text . '</a></li>';
-    
-    if (THIS_PAGE==$url){
-        echo '<li class="nav-item active px-lg-4">
-    <a class="nav-link text-uppercase text-expanded" href="' . $url . '">' . $text . '
-    <span class="sr-only">(current)</span>
-    </a>
-</li>';
-        
-    }else{
-        
-    echo '
-    <li class="nav-item px-lg-4">
-        <a class="nav-link text-uppercase text-expanded" href="' . $url . '">' . $text . '</a>
-    </li>';
-}
-
-}
-}
-
+//START NEW THEME STUFF
+//creates theme virtual path for theme assets, JS, CSS, images
+$config->theme_virtual = $config->virtual_path . 'themes/' . $config->theme . '/';
+//END NEW THEME STUFF
 
 /*
-<li class="nav-item active px-lg-4">
-    <a class="nav-link text-uppercase text-expanded" href="template.php">Home
-    <span class="sr-only">(current)</span>
-    </a>
-</li>
-<li class="nav-item px-lg-4">
-    <a class="nav-link text-uppercase text-expanded" href="#">About</a>
-</li>
-<li class="nav-item px-lg-4">
-    <a class="nav-link text-uppercase text-expanded" href="#">Products</a>
-</li>
-<li class="nav-item px-lg-4">
-    <a class="nav-link text-uppercase text-expanded" href="#">Store</a>
-</li>
-<li class="nav-item px-lg-4">
-    <a class="nav-link text-uppercase text-expanded" href="email1.php">Simple Email Form</a>
-</li>
-<li class="nav-item px-lg-4">
-    <a class="nav-link text-uppercase text-expanded" href="email2.php">Compound Email Form</a>
-</li>
-
+ * adminWidget allows clients to get to admin page from anywhere
+ * code will show/hide based on logged in status
 */
-
-
+/*
+ * adminWidget allows clients to get to admin page from anywhere
+ * code will show/hide based on logged in status
+*/
+if(startSession() && isset($_SESSION['AdminID']))
+{#add admin logged in info to sidebar or nav
+    
+    $config->adminWidget = '
+        <a href="' . ADMIN_PATH . 'admin_dashboard.php">ADMIN</a> 
+        <a href="' . ADMIN_PATH . 'admin_logout.php">LOGOUT</a>
+    ';
+}else{//show login (YOU MAY WANT TO SET TO EMPTY STRING FOR SECURITY)
+    
+    $config->adminWidget = '
+        <a  href="' . ADMIN_PATH . 'admin_login.php">LOGIN</a>
+    ';
+}
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
