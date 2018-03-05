9bd27201cc5035b5fa43eaecacc398277fe30085
Eddy Matlock-Mahon <36487159+eddymahon82@users.noreply.github.com>
2018-03-05 12:05:03 -0800

    Delete credentials.php

--- widgets2/includes/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-//credentials.php
-
-define('DB_NAME','scc_mysql');
-define('DB_USER','sccdesignsbyeddy');
-define('DB_PASSWORD','Champion1982#');
-define('DB_HOST','mysql.designsbyeddy.tech');
